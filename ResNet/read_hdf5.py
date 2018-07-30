1e5c6e40fb8f6b69d855b254fc5b22cd4d644cb6
Liu <liuy08@mcrc13.msb>
2018-07-30 17:13:06 -0400

    updated .gitignore file. Before push to github

--- ResNet/read_hdf5.py
+++ ResNet/read_hdf5.py
@@ -1,30 +1,28 @@
 # -*- coding: utf-8 -*-
 
-import numpy as np
 import os, h5py
 from random import shuffle
 import matplotlib.pyplot as plt
 
-def read_hdf5(path, batch_size = 32):
+def read_hdf5(path, dataset="train", batch_size = 32):
     """
     """
     
     hdf5_file = h5py.File(path, mode='r')
+    data_img = dataset + "_img"
+    data_labels = dataset + "_labels"
     
-    train_mean = hdf5_file["train_mean"][...]
-    train_mean = train_mean[np.newaxis, ...]
+    m_data = hdf5_file[data_img].shape[0]
     
-    m_train = hdf5_file["train_img"].shape[0]
-    
-    batch_list = list(range(int(m_train / batch_size) + 1))
+    batch_list = list(range(int(m_data / batch_size) + 1))
     shuffle(batch_list)
     
     for idx, num in enumerate(batch_list):
         n_start = num * batch_size
-        n_end = min((num + 1) * batch_size, m_train)
+        n_end = min((num + 1) * batch_size, m_data)
         
-        images = hdf5_file["train_img"][n_start:n_end, ...]
-        labels = hdf5_file["train_labels"][n_start:n_end]
+        images = hdf5_file[data_img][n_start:n_end, ...]
+        labels = hdf5_file[data_labels][n_start:n_end]
         
         yield images, labels
         
@@ -35,6 +33,12 @@ def read_hdf5(path, batch_size = 32):
 if __name__ == "__main__":
     path = os.path.join("..", "data", "76_79_80.hdf5")
     for image, label in read_hdf5(path):
+        if len(label) < 32:
+            print("Label: ", str(label))
+            plt.imshow(image[0])
+            plt.show()
+            
+    for image, label in read_hdf5(path, "test"):
         if len(label) < 32:
             print("Label: ", str(label))
             plt.imshow(image[0])
