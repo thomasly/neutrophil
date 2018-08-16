c98d4800acdac2363df2cb38120e95db5a0a2a3e
Yang Liu <thomasliuy@gmail.com>
2018-08-16 11:55:00 -0400

    use keras pretrained inceptionresnetv2

--- ResNet/read_hdf5.py
+++ ResNet/read_hdf5.py
@@ -4,6 +4,7 @@ import os, h5py
 from random import shuffle
 import matplotlib.pyplot as plt
 import numpy as np
+from tensorflow.keras.utils import to_categorical
 
 def read_hdf5(hdf5_file, dataset="train", batch_size=32):
     """
@@ -13,15 +14,15 @@ def read_hdf5(hdf5_file, dataset="train", batch_size=32):
     data_labels = dataset + "_labels"
     
     m_data = hdf5_file[data_img].shape[0]
-    ind = list(range(m_data))
+    ix = list(range(m_data))
     
     while True:
-        shuffle(ind)
+        shuffle(ix)
         inputs = []
         targets = []
         for i in range(m_data):
-            inputs.append(hdf5_file[data_img][ind[i]])
-            targets.append(hdf5_file[data_labels][ind[i]])
+            inputs.append(hdf5_file[data_img][ix[i]])
+            targets.append(to_categorical(hdf5_file[data_labels][ix[i]], num_classes=2))
             if (i+1) % batch_size == 0 or (i+1) == m_data:
                 inputs = np.stack(inputs)
                 targets = np.stack(targets)
---
+++ ResNet/transfer_training_InceptionResNetV2.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+
+from tensorflow.keras.applications.inception_resnet_v2 import InceptionResNetV2
+import datetime, os, tables
+from math import ceil
+from LossHistory import LossHistory
+from read_hdf5 import read_hdf5
+from tensorflow.keras.utils import to_categorical
+
+def train(batch_size = 32, epochs = 15, validation = True):
+    """
+    """
+    
+    start_time = datetime.now()
+    hdf5_path = os.path.join("..", "data", "76_79_80.hdf5")
+    model = InceptionResNetV2(classes = 2)
+    model.compile(
+            optimizer = "adam", 
+            loss = "binary_crossentropy", 
+            metrics = ["accuracy"]
+            )
+    
+    hdf5_file = tables.open_file(hdf5_path, mode = 'r')
+    n_train = hdf5_file.root.train_img.shape[0]
+    n_test = hdf5_file.root.test_img.shape[0]
+    
+    steps_per_epoch = int(ceil(n_train / batch_size))
+    validation_steps = int(ceil(n_test / batch_size))
+
+    history = LossHistory('epoch_loss.log', 'batch_loss.log')
+    
+    try:
+        model.fit_generator(
+                read_hdf5(
+                        hdf5_file, 
+                        batch_size = batch_size,
+                        ), 
+                steps_per_epoch = steps_per_epoch,
+                epochs = epochs,
+                verbose = 2, 
+                callbacks = [history],
+                validation_data = read_hdf5(
+                        hdf5_file, 
+                        dataset = "test", 
+                        batch_size = batch_size,
+                        ),
+                validation_steps = validation_steps
+                )
+        
+        if validation:
+            eva_data = hdf5_file.root.val_img
+            eva_labels = hdf5_file.root.val_labels
+            eva_labels = to_categorical(eva_labels, num_classes=2)
+            print("Validation data shape: {}".format(str(eva_data.shape)))
+            print("validation labels shape: {}".format(str(eva_labels.shape)))
+            preds = model.evaluate(eva_data, eva_labels)
+            print("Validation loss: {}".format(preds[0]))
+            print("Validation accuracy: {}".format(preds[1]))
+        
+        hdf5_file.close()
+
+    except StopIteration:
+        
+        hdf5_file.close()
+        
+    finally:
+        
+        hdf5_file.close()
+        
+    print("Training time: ", datetime.now() - start_time)
