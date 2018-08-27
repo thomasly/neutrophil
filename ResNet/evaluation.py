f10ed137339b0b1afb65aa94c86b37d17bedeb94
Yang Liu <thomasliuy@gmail.com>
2018-08-27 16:03:46 -0400

    evaluation.py works now

--- ResNet/evaluation.py
+++ ResNet/evaluation.py
@@ -1,11 +1,25 @@
 import os, sys, tables
 from tensorflow.keras.models import model_from_json
 from math import ceil
-from read_hdf5 import read_hdf5
-import matplotlib
-matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
+plt.switch_backend('agg')
 from sklearn.metrics import roc_curve, auc
+
+def read_hdf5(hdf5_file, dataset="train", batch_size=32):
+    """
+    """
+    
+    data_img = dataset + "_img"
+    
+    m_data = hdf5_file.root.__getitem__(data_img).shape[0]
+    batch_list = list(range(int(ceil(m_data / batch_size))))
+    
+    while True:
+        for num in batch_list:
+            n_start = num * batch_size
+            n_end = min((num + 1) * batch_size, m_data)
+            inputs = hdf5_file.root.__getitem__(data_img)[n_start:n_end]
+            yield inputs
 
 def evaluate_model(json_file, weights, pred_file):
     """
@@ -31,11 +45,14 @@ def evaluate_model(json_file, weights, pred_file):
 
         preds = model.predict_generator(generator, steps=steps, verbose=1)
         true_values = hdf5_file.root.val_labels
-
+        print("preds: ", preds[0:10])
+        print("true_values: ", true_values[0:10])
         fpr, tpr, _ = roc_curve(list(true_values), list(preds))
+        print("fpr: ", fpr[0:10])
+        print("tpr: ", tpr[0:10])
         roc_auc = auc(fpr, tpr)
 
-        plt.figure()
+        fig = plt.figure()
         lw = 2
         plt.plot(fpr, tpr, color='darkorange',
                 lw=lw, label='ROC curve (area = %0.2f)' % roc_auc)
@@ -46,7 +63,7 @@ def evaluate_model(json_file, weights, pred_file):
         plt.ylabel('True Positive Rate')
         plt.title('Neutrophil Identifier ROC')
         plt.legend(loc="lower right")
-        plt.show()
+        fig.savefig("aoc.png", dpi=fig.dpi)
 
     finally:
         hdf5_file.close()
