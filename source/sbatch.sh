6f3869610b6cf3bf00139b6173c5b3e69e4cf750
Yang Liu <thomasliuy@gmail.com>
2018-09-12 11:56:18 -0400

    fix sbatch.sh

--- source/sbatch.sh
+++ source/sbatch.sh
@@ -12,20 +12,5 @@
 
 module purge
 module add python/gpu/3.6.5
-#module add cuda91/blas/9.1.85
-#module add cuda91/fft/9.1.85
-#module add cuda91/nsight/9.1.85
-#module add cuda91/profiler/9.1.85
-#module add cuda91/toolkit/9.1.85
-#module add cuda90/blas/9.0.176
-#module add cuda90/fft/9.0.176
-#module add cuda90/nsight/9.0.176
-#module add cuda90/profiler/9.0.176
-#module add cuda90/toolkit/9.0.176
-#module add cuda80/blas/8.0.61
-#module add cuda80/fft/8.0.61
-#module add cuda80/nsight/8.0.61
-#module add cuda80/profiler/8.0.61
-#module add cuda80/toolkit/8.0.61
 wait
 python $1 -n $2 -b $3 -e $4 -g $5 -c $6 -v
