080211e152cf96dfb15b403afba92d97fa6d70f2
Yang Liu <thomasliuy@gmail.com>
2018-10-04 14:59:44 -0400

    restructure the code

--- source/evaluation.py
+++ source/evaluation.py
@@ -5,7 +5,7 @@ Project: Neutrophil Identifier
 Author: Yang Liu
 Created date: Sep 5, 2018 4:13 PM
 -----
-Last Modified: Oct 4, 2018 2:40 PM
+Last Modified: Oct 4, 2018 2:53 PM
 Modified By: Yang Liu
 -----
 License: MIT
@@ -43,6 +43,54 @@ def read_hdf5(hdf5_file, dataset, batch_size):
             yield inputs
 
 
+def plot_roc(fpr, tpr, roc_auc):
+    '''plot and save roc'''
+    fig = plt.figure()
+    lw = 2
+    plt.plot(
+        fpr, tpr, color='darkorange',
+        lw=lw, label='ROC curve (area = %0.2f)' % roc_auc
+    )
+    plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Neutrophil Identifier ROC')
+    plt.legend(loc="lower right")
+    fig.savefig("aoc.png", dpi=fig.dpi)
+
+
+def plot_prc(recall, precision, average_precision):
+    '''plot and save prc'''
+    fig = plt.figure()
+    step_kwargs = (
+        {'step': 'post'}
+        if 'step' in signature(plt.fill_between).parameters
+        else {}
+    )
+    plt.step(
+        recall, precision, color='b', alpha=0.2,
+        where='post'
+    )
+    plt.fill_between(
+        recall, precision, alpha=0.2, color='b', **step_kwargs
+    )
+    plt.xlabel('Recall')
+    plt.ylabel('Precision')
+    plt.ylim([0.0, 1.05])
+    plt.xlim([0.0, 1.0])
+    plt.title('2-class Precision-Recall curve: AP={0:0.2f}'.format(
+            average_precision))
+    fig.savefig("prc.png", dpi=fig.dpi)
+
+
+def clear_plot():
+    plt.clf()
+    plt.cla()
+    plt.close()
+
+
 def evaluate_model(h5_file, pred_file):
     """
     evaluate the trained model. Plot ROC curve and calculate AUC.
@@ -68,51 +116,13 @@ def evaluate_model(h5_file, pred_file):
         true_values = hdf5_file.root.test_labels
         fpr, tpr, _ = roc_curve(list(true_values), list(preds))
         precision, recall, thresholds = prc(list(true_values), list(preds))
-        average_precision = average_precision_score(list(true_values), list(preds))
+        average_precision = average_precision_score(
+            list(true_values), list(preds))
         roc_auc = auc(fpr, tpr)
 
-        # plot and save roc
-        fig = plt.figure()
-        lw = 2
-        plt.plot(
-            fpr, tpr, color='darkorange',
-            lw=lw, label='ROC curve (area = %0.2f)' % roc_auc
-        )
-        plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
-        plt.xlim([0.0, 1.0])
-        plt.ylim([0.0, 1.05])
-        plt.xlabel('False Positive Rate')
-        plt.ylabel('True Positive Rate')
-        plt.title('Neutrophil Identifier ROC')
-        plt.legend(loc="lower right")
-        fig.savefig("aoc.png", dpi=fig.dpi)
-
-        # clear plot
-        plt.clf()
-        plt.cla()
-        plt.close()
-
-        # plot and save prc
-        fig = plt.figure()
-        step_kwargs = (
-            {'step': 'post'}
-            if 'step' in signature(plt.fill_between).parameters
-            else {}
-        )
-        plt.step(
-            recall, precision, color='b', alpha=0.2,
-            where='post'
-        )
-        plt.fill_between(
-            recall, precision, alpha=0.2, color='b', **step_kwargs
-        )
-        plt.xlabel('Recall')
-        plt.ylabel('Precision')
-        plt.ylim([0.0, 1.05])
-        plt.xlim([0.0, 1.0])
-        plt.title('2-class Precision-Recall curve: AP={0:0.2f}'.format(
-                average_precision))
-        fig.savefig("prc.png", dpi=fig.dpi)
+        plot_roc(fpr, tpr, roc_auc)
+        clear_plot()
+        plot_prc(recall, precision, average_precision)
 
     finally:
         hdf5_file.close()
