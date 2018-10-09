c93a994468fd82ce7386be0b238b141e10457e30
Yang Liu <thomasliuy@gmail.com>
2018-10-09 11:31:05 -0400

    make paths Paths' class varialbes

--- source/paths.py
+++ source/paths.py
@@ -1,32 +1,30 @@
-import os, sys
+import os
+
 
 class Paths:
 
-    def __init__(self):
+    # home directory
+    source = os.path.dirname(os.path.abspath(__file__))
+    home = os.path.dirname(source)
 
-        # home directory
-        self.source = os.path.dirname(os.path.abspath(__file__))
-        self.home = os.path.dirname(self.source)
+    # data directory
+    data = os.path.join(home, "data")
+    data_orig = os.path.join(data, "orig")
+    data_pool = os.path.join(data, "pool")
+    data_predict = os.path.join(data, "predict")
+    data_test = os.path.join(data, "test")
+    data_orig_76_79 = os.path.join(data_orig, "76_79")
+    neg_76_79 = os.path.join(data_orig_76_79, "neg")
+    pos_76_79 = os.path.join(data_orig_76_79, "pos")
+    data_orig_80 = os.path.join(data_orig, "80")
+    neg_80 = os.path.join(data_orig_80, "neg")
+    pos_80 = os.path.join(data_orig_80, "pos")
+    slice_80 = os.path.join(data_orig, "ImageCollection_80.scn")
+    hdf5_76_79_80 = os.path.join(data, "76_79_80.hdf5")
+    tiles_80 = os.path.join(data_test, "tiles_80.hdf5")
 
-        # data directory
-        self.data = os.path.join(self.home, "data")
-        self.data_orig = os.path.join(self.data, "orig")
-        self.data_pool = os.path.join(self.data, "pool")
-        self.data_predict = os.path.join(self.data, "predict")
-        self.data_test = os.path.join(self.data, "test")
-        self.data_orig_76_79 = os.path.join(self.data_orig, "76_79")
-        self.neg_76_79 = os.path.join(self.data_orig_76_79, "neg")
-        self.pos_76_79 = os.path.join(self.data_orig_76_79, "pos")
-        self.data_orig_80 = os.path.join(self.data_orig, "80")
-        self.neg_80 = os.path.join(self.data_orig_80, "neg")
-        self.pos_80 = os.path.join(self.data_orig_80, "pos")
-        self.slice_80 = os.path.join(self.data_orig, "ImageCollection_80.scn")
-        self.hdf5_76_79_80 = os.path.join(self.data, "76_79_80.hdf5")
-        self.tiles_80 = os.path.join(self.data_test, "tiles_80.hdf5")
+    # log directory
+    logs = os.path.join(source, "logs")
 
-        # log directory
-        self.logs = os.path.join(self.source, "logs")
-        self.pred_cvs = os.path.join(self.data_test, "predictions.csv")
-
-        # model directory
-        self.models = os.path.join(self.source, "models")
+    # model directory
+    models = os.path.join(source, "models")
